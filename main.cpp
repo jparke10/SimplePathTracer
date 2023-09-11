d1d4bdfcff880c436bde072f1afa892c7c0be019
jparke10 <129916202+jparke10@users.noreply.github.com>
2023-09-11 14:56:18 -0700

    implemented progress indicator and execution time

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 int main() {
     // Image
@@ -9,7 +10,9 @@ int main() {
     // Columns, then rows, then the max color value
     // Pixels are RGB triplets
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    auto start = std::chrono::system_clock::now();
     for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rLines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
@@ -22,4 +25,7 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    auto end = std::chrono::system_clock::now();
+    std::clog << "\rFinished render in " <<
+    std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms\n";
 }
