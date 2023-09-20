1c34234af7956ffd80b8831a39f3406a075b27d8
jparke10 <129916202+jparke10@users.noreply.github.com>
2023-09-20 13:56:39 -0700

    added surface side determination to hittable

--- hittable.h
+++ hittable.h
@@ -8,6 +8,15 @@ class HitRecord {
         Point3 p;
         Vec3 normal;
         double t;
+        bool front_face;
+
+        void set_face_normal(const Ray& r, const Vec3& outward_normal) {
+            // normals will always face against ray, so need to determine front/back for render
+            // sets the hitrecord normal vector
+            // the parameter "outward_normal" assumed to be unit length
+            front_face = dot(r.direction(), outward_normal) < 0;
+            normal = front_face ? outward_normal : -outward_normal;
+        }
 };
 
 class Hittable {
--- sphere.h
+++ sphere.h
@@ -28,7 +28,8 @@ class Sphere : public Hittable {
             }
             rec.t = root;
             rec.p = r.at(rec.t);
-            rec.normal = (rec.p - center) / radius;
+            Vec3 outward_normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, outward_normal);
             return true;
         }
 };
