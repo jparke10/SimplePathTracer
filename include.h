7734b3b953a13507f02e5312ef679694b12443d2
jparke10 <129916202+jparke10@users.noreply.github.com>
2023-09-20 14:48:52 -0700

    added file for constants

---
+++ include.h
@@ -0,0 +1,26 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+// usings
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// utility function(s)
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.;
+}
+
+// common headers
+#include "ray.h"
+#include "vec3.h"
+
+#endif
